1d45f3bb464fbafbbcb007eb8455d06c874759a7
Gabriel Bernardi <gabriel.s.bernardi@gmail.com>
2019-04-29 14:25:50 -0300

    :rocket: Meu primeiro deploy no Heroku!

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-path = require('path');
-
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
 const app = express();
-app.use(express.static('./dist/hotel-bernardi'));
-app.length('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/hotel-bernardi/index.html'))
-
-    app.listen(process.env.PORT || 8080, () => {
-        conseole.log("Server start")
-    })
+ 
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+ 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
+ 
+app.listen(process.env.PORT || 8080);
